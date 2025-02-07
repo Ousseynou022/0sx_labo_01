ab82be59b23cb98bf36bbe39c19309bb8121e2ef
Ousseynou022 <bayeousseynoud@gmail.com>
2025-02-07 14:10:55 -0500

    fini

--- 0sx_labo_01/0sx_labo_01.ino
+++ 0sx_labo_01/0sx_labo_01.ino
@@ -1,9 +1,46 @@
-void setup() {
-  // put your setup code here, to run once:
+int led = 13;
+int nbrPas = 256;
+int time = 2048 / nbrPas;
+int delai = 2000;
 
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
 }
+void allumer() {
+  digitalWrite(led, HIGH);
+  Serial.println("Allume-2419796");
+  delay(delai);
+}
+
+void clignotement() {
+  int delai = 250;
+  for (int i = 0; i < 6; i++) {
+    digitalWrite(led, HIGH);
+    delay(delai);
+    digitalWrite(led, LOW);
+    delay(delai);
+  }
+  Serial.println("Clignote-2419796");
+}
+void variation() {
+  for (int i = 1; i < nbrPas; i++) {
+    analogWrite(led, i);
+    delay(time);
+  }
+}
+void eteindre() {
+  digitalWrite(led, LOW);
+  delay(delai);
+}
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  allumer();
+  clignotement();
+  variation();
+  eteindre();
 }
